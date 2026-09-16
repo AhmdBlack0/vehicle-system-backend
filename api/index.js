6af67a83e6da21283e06113f5a4c7c18eeaa0ac4
AhmdBlack0 <AhmdBlack.0@gmail.com>
2026-09-17 00:12:52 +0300

    Add debug middleware to api/index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,17 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
+// Debug middleware
+app.use((req, res, next) => {
+  console.log('=== API INDEX DEBUG ===');
+  console.log('Method:', req.method);
+  console.log('Path:', req.path);
+  console.log('Content-Type:', req.get('Content-Type'));
+  console.log('Body:', req.body);
+  console.log('Body type:', typeof req.body);
+  next();
+});
+
 // Import and use the main app as middleware
 const mainApp = require('../index');
 app.use(mainApp);
